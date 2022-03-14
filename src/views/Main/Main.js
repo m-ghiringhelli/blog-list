d7f1c1a2ed30293228f6597f786fbcd7655d7ef5
m-ghiringhelli <mghiring@gmail.com>
2022-03-14 12:51:50 -0700

    successfully console logging blog data

--- src/views/Main/Main.js
+++ src/views/Main/Main.js
@@ -1,7 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchBlogs } from '../../services/blogs'; 
 
 export default function Main() {
   const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const resp = await fetchBlogs();
+      console.log(resp);
+    };
+    fetchData();
+  }, []);
 
   return (
     <div>Main</div>
